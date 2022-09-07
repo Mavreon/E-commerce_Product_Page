d53ed9c4a9f9235167a47219c0134544fbd28b94
Chisom Ogbonna <mavreon@gmail.com>
2022-09-07 22:23:28 +0100

    Created Navbar
    Created navbar and added some required components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,57 +1,26 @@
-import React, {useState,useRef} from "react";
-import styles from './Navbar.module.css';
-import cartIcon from '../../images/icon-cart.svg';
-import avatar from '../../images/image-avatar.png';
-import menuIcon from '../../images/icon-menu.svg';
-import closeIcon from '../../images/icon-close.svg';
+import React from 'react'
+import styles from './Navbar.module.css'
+import Wrapper from '../UI/Wrapper'
+import iconMenu from '../../images/icon-menu.svg'
 
-const Navbar = (props)=>{
-    const navBtnRef = useRef();
-    const [backdropClasses, setBackdropClasses] = useState(styles.backdrop);
-    const [navbarClasses, setNavbarClasses] = useState(styles.navbar);
-    const [isClicked, setIsClicked] = useState(false);
-
-    const onClickNavButtonHandler = ()=>{
-        if(!isClicked)
-        {
-            setIsClicked(true);
-            navBtnRef.current.src = closeIcon;
-            setBackdropClasses(`${styles.backdrop} ${styles.active}`);
-            setNavbarClasses(`${styles.navbar} ${styles.active}`);
-        }
-        else{
-            setIsClicked(false);
-            navBtnRef.current.src = menuIcon;
-            setBackdropClasses(`${styles.backdrop}`);
-            setNavbarClasses(`${styles.navbar}`);
-        }
-    }
-
+const Navbar = ()=>{
     return(
-        <header className={styles.header}>
-            <div className={styles["nav-btn"]}>
-                <img className={styles["nav-btn__open"]} src={menuIcon} onClick={onClickNavButtonHandler} ref={navBtnRef}/>
-            </div>
-            <div className={`${backdropClasses}`}/>
-            <h2 className={styles.logo}>Sneakers</h2>
-            <nav className={navbarClasses}>
-                <ul className={styles["nav-links"]}>
-                    <li><a className={styles["nav-link"]} href="#">Collection</a></li>
-                    <li><a className={styles["nav-link"]} href="#">Men</a></li>
-                    <li><a className={styles["nav-link"]} href="#">Women</a></li>
-                    <li><a className={styles["nav-link"]} href="#">About</a></li>
-                    <li><a className={styles["nav-link"]} href="#">Contact</a></li>
-                </ul>
-            </nav>
-            <div className={styles.profile}>
-                <div className={styles.cart}>
-                    <span className={`${styles.count} ${styles.active}`}>1</span>
-                    <img className={styles["cart-icon"]} src = {cartIcon}/>
-                </div>
-                <div className={styles["profile-image"]}><img src={avatar}/></div>
-            </div>
-        </header>
-    );
+        <nav className={`${styles.navbar}`}>
+            <Wrapper style={{gap: '6rem'}}>
+            <Wrapper>
+                <div className={`${styles['navbar__btn']}`}><img src={iconMenu} alt='Sneakers Logo'/></div>
+                <h1 className={`${styles['navbar__logo']}`}>sneakers</h1>
+            </Wrapper>
+            <ul className={`${styles['navbar__links']}`}>
+                <li className={`${styles['navbar__link']}`}><a href='/'>Collections</a></li>
+                <li className={`${styles['navbar__link']}`}><a href='/'>Men</a></li>
+                <li className={`${styles['navbar__link']}`}><a href='/'>Women</a></li>
+                <li className={`${styles['navbar__link']}`}><a href='/'>About</a></li>
+                <li className={`${styles['navbar__link']}`}><a href='/'>Contact</a></li>
+            </ul>
+            </Wrapper>
+        </nav>
+    )
 }
 
-export default Navbar;
+export default Navbar
